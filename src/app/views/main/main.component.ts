cdcae64e4997015328726208d39f0fae44e73a88
autumn2014 <283139246@qq.com>
2021-06-28 16:31:37 +0800

    格式

--- src/app/views/main/main.component.ts
+++ src/app/views/main/main.component.ts
@@ -15,12 +15,14 @@ export class MainComponent implements OnInit {
    */
   constructor(private cartService:CartServiceService) {}
   hero:Hero={id:100,name:'raoxiaokang'};
-  person:Person[]=[{id:200,name:'rxk',addr:'wuhan'},
-  {id:200,name:'rxk',addr:'bj'},
-  {id:200,name:'rxk',addr:'sh'},
-  {id:200,name:'rxk',addr:'wc'}];
-  ngOnInit(): void {
+  person:Person[]=[
+    {id:200,name:'rxk',addr:'wuhan'},
+    {id:200,name:'rxk',addr:'bj'},
+    {id:200,name:'rxk',addr:'sh'},
+    {id:200,name:'rxk',addr:'wc'}
+  ];
 
+  ngOnInit(): void {
     console.log(this.hero);
     console.log(this.person)
     console.log(this.cartService.getItems());
